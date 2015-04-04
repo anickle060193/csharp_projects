9b82de27c86ab3a07f4837f078adf71fe59cd0f8
Adam Nickle <anickle060193@gmail.com>
2015-04-04 13:54:49 -0500

    Committing Test Twist.

--- MyGamesLibrary/MyGamesLibrary/Games/TextTwistForm.Designer.cs
+++ MyGamesLibrary/MyGamesLibrary/Games/TextTwistForm.Designer.cs
@@ -36,12 +36,12 @@
             this.uxLetter6 = new System.Windows.Forms.TextBox();
             this.uxLettersGroup = new System.Windows.Forms.GroupBox();
             this.uxWordGroup = new System.Windows.Forms.GroupBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.uxWordLetter1 = new System.Windows.Forms.TextBox();
+            this.uxWordLetter2 = new System.Windows.Forms.TextBox();
+            this.uxWordLetter3 = new System.Windows.Forms.TextBox();
+            this.uxWordLetter4 = new System.Windows.Forms.TextBox();
+            this.uxWordLetter6 = new System.Windows.Forms.TextBox();
+            this.uxWordLetter5 = new System.Windows.Forms.TextBox();
             this.uxWordsList = new System.Windows.Forms.ListBox();
             this.uxLettersGroup.SuspendLayout();
             this.uxWordGroup.SuspendLayout();
@@ -49,69 +49,81 @@
             // 
             // uxLetter1
             // 
+            this.uxLetter1.Enabled = false;
             this.uxLetter1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.uxLetter1.Location = new System.Drawing.Point(6, 19);
             this.uxLetter1.MaxLength = 1;
             this.uxLetter1.Name = "uxLetter1";
-            this.uxLetter1.ReadOnly = true;
             this.uxLetter1.Size = new System.Drawing.Size(62, 62);
             this.uxLetter1.TabIndex = 0;
+            this.uxLetter1.TabStop = false;
             this.uxLetter1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.uxLetter1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextTwist_KeyPress);
             // 
             // uxLetter2
             // 
+            this.uxLetter2.Enabled = false;
             this.uxLetter2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.uxLetter2.Location = new System.Drawing.Point(74, 19);
             this.uxLetter2.MaxLength = 1;
             this.uxLetter2.Name = "uxLetter2";
-            this.uxLetter2.ReadOnly = true;
             this.uxLetter2.Size = new System.Drawing.Size(62, 62);
             this.uxLetter2.TabIndex = 1;
+            this.uxLetter2.TabStop = false;
             this.uxLetter2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.uxLetter2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextTwist_KeyPress);
             // 
             // uxLetter4
             // 
+            this.uxLetter4.Enabled = false;
             this.uxLetter4.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.uxLetter4.Location = new System.Drawing.Point(210, 19);
             this.uxLetter4.MaxLength = 1;
             this.uxLetter4.Name = "uxLetter4";
-            this.uxLetter4.ReadOnly = true;
             this.uxLetter4.Size = new System.Drawing.Size(62, 62);
             this.uxLetter4.TabIndex = 3;
+            this.uxLetter4.TabStop = false;
             this.uxLetter4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.uxLetter4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextTwist_KeyPress);
             // 
             // uxLetter3
             // 
+            this.uxLetter3.Enabled = false;
             this.uxLetter3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.uxLetter3.Location = new System.Drawing.Point(142, 19);
             this.uxLetter3.MaxLength = 1;
             this.uxLetter3.Name = "uxLetter3";
-            this.uxLetter3.ReadOnly = true;
             this.uxLetter3.Size = new System.Drawing.Size(62, 62);
             this.uxLetter3.TabIndex = 2;
+            this.uxLetter3.TabStop = false;
             this.uxLetter3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.uxLetter3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextTwist_KeyPress);
             // 
             // uxLetter5
             // 
+            this.uxLetter5.Enabled = false;
             this.uxLetter5.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.uxLetter5.Location = new System.Drawing.Point(278, 19);
             this.uxLetter5.MaxLength = 1;
             this.uxLetter5.Name = "uxLetter5";
-            this.uxLetter5.ReadOnly = true;
             this.uxLetter5.Size = new System.Drawing.Size(62, 62);
             this.uxLetter5.TabIndex = 5;
+            this.uxLetter5.TabStop = false;
             this.uxLetter5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.uxLetter5.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextTwist_KeyPress);
             // 
             // uxLetter6
             // 
+            this.uxLetter6.Enabled = false;
             this.uxLetter6.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.uxLetter6.Location = new System.Drawing.Point(346, 19);
             this.uxLetter6.MaxLength = 1;
             this.uxLetter6.Name = "uxLetter6";
-            this.uxLetter6.ReadOnly = true;
             this.uxLetter6.Size = new System.Drawing.Size(62, 62);
             this.uxLetter6.TabIndex = 4;
+            this.uxLetter6.TabStop = false;
             this.uxLetter6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.uxLetter6.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextTwist_KeyPress);
             // 
             // uxLettersGroup
             // 
@@ -121,101 +133,123 @@
             this.uxLettersGroup.Controls.Add(this.uxLetter4);
             this.uxLettersGroup.Controls.Add(this.uxLetter6);
             this.uxLettersGroup.Controls.Add(this.uxLetter5);
-            this.uxLettersGroup.Location = new System.Drawing.Point(312, 103);
+            this.uxLettersGroup.Location = new System.Drawing.Point(311, 234);
             this.uxLettersGroup.Name = "uxLettersGroup";
             this.uxLettersGroup.Size = new System.Drawing.Size(417, 97);
             this.uxLettersGroup.TabIndex = 12;
             this.uxLettersGroup.TabStop = false;
-            this.uxLettersGroup.Text = "Word";
+            this.uxLettersGroup.Text = "Letters";
             // 
             // uxWordGroup
             // 
-            this.uxWordGroup.Controls.Add(this.textBox1);
-            this.uxWordGroup.Controls.Add(this.textBox2);
-            this.uxWordGroup.Controls.Add(this.textBox3);
-            this.uxWordGroup.Controls.Add(this.textBox4);
-            this.uxWordGroup.Controls.Add(this.textBox5);
-            this.uxWordGroup.Controls.Add(this.textBox6);
-            this.uxWordGroup.Location = new System.Drawing.Point(312, 206);
+            this.uxWordGroup.Controls.Add(this.uxWordLetter1);
+            this.uxWordGroup.Controls.Add(this.uxWordLetter2);
+            this.uxWordGroup.Controls.Add(this.uxWordLetter3);
+            this.uxWordGroup.Controls.Add(this.uxWordLetter4);
+            this.uxWordGroup.Controls.Add(this.uxWordLetter6);
+            this.uxWordGroup.Controls.Add(this.uxWordLetter5);
+            this.uxWordGroup.Location = new System.Drawing.Point(311, 131);
             this.uxWordGroup.Name = "uxWordGroup";
             this.uxWordGroup.Size = new System.Drawing.Size(417, 97);
             this.uxWordGroup.TabIndex = 13;
             this.uxWordGroup.TabStop = false;
-            this.uxWordGroup.Text = "Letters";
+            this.uxWordGroup.Text = "Word";
             // 
-            // textBox1
+            // uxWordLetter1
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(6, 19);
-            this.textBox1.MaxLength = 1;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(62, 62);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.uxWordLetter1.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.uxWordLetter1.Enabled = false;
+            this.uxWordLetter1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxWordLetter1.Location = new System.Drawing.Point(6, 19);
+            this.uxWordLetter1.MaxLength = 1;
+            this.uxWordLetter1.Name = "uxWordLetter1";
+            this.uxWordLetter1.Size = new System.Drawing.Size(62, 62);
+            this.uxWordLetter1.TabIndex = 0;
+            this.uxWordLetter1.TabStop = false;
+            this.uxWordLetter1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.uxWordLetter1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextTwist_KeyPress);
             // 
-            // textBox2
+            // uxWordLetter2
             // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(74, 19);
-            this.textBox2.MaxLength = 1;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(62, 62);
-            this.textBox2.TabIndex = 1;
-            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.uxWordLetter2.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.uxWordLetter2.Enabled = false;
+            this.uxWordLetter2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxWordLetter2.Location = new System.Drawing.Point(74, 19);
+            this.uxWordLetter2.MaxLength = 1;
+            this.uxWordLetter2.Name = "uxWordLetter2";
+            this.uxWordLetter2.Size = new System.Drawing.Size(62, 62);
+            this.uxWordLetter2.TabIndex = 1;
+            this.uxWordLetter2.TabStop = false;
+            this.uxWordLetter2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.uxWordLetter2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextTwist_KeyPress);
             // 
-            // textBox3
+            // uxWordLetter3
             // 
-            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(142, 19);
-            this.textBox3.MaxLength = 1;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.ReadOnly = true;
-            this.textBox3.Size = new System.Drawing.Size(62, 62);
-            this.textBox3.TabIndex = 2;
-            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.uxWordLetter3.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.uxWordLetter3.Enabled = false;
+            this.uxWordLetter3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxWordLetter3.Location = new System.Drawing.Point(142, 19);
+            this.uxWordLetter3.MaxLength = 1;
+            this.uxWordLetter3.Name = "uxWordLetter3";
+            this.uxWordLetter3.Size = new System.Drawing.Size(62, 62);
+            this.uxWordLetter3.TabIndex = 2;
+            this.uxWordLetter3.TabStop = false;
+            this.uxWordLetter3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.uxWordLetter3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextTwist_KeyPress);
             // 
-            // textBox4
+            // uxWordLetter4
             // 
-            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox4.Location = new System.Drawing.Point(210, 19);
-            this.textBox4.MaxLength = 1;
-            this.textBox4.Name = "textBox4";
-            this.textBox4.ReadOnly = true;
-            this.textBox4.Size = new System.Drawing.Size(62, 62);
-            this.textBox4.TabIndex = 3;
-            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.uxWordLetter4.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.uxWordLetter4.Enabled = false;
+            this.uxWordLetter4.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxWordLetter4.Location = new System.Drawing.Point(210, 19);
+            this.uxWordLetter4.MaxLength = 1;
+            this.uxWordLetter4.Name = "uxWordLetter4";
+            this.uxWordLetter4.Size = new System.Drawing.Size(62, 62);
+            this.uxWordLetter4.TabIndex = 3;
+            this.uxWordLetter4.TabStop = false;
+            this.uxWordLetter4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.uxWordLetter4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextTwist_KeyPress);
             // 
-            // textBox5
+            // uxWordLetter6
             // 
-            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox5.Location = new System.Drawing.Point(346, 19);
-            this.textBox5.MaxLength = 1;
-            this.textBox5.Name = "textBox5";
-            this.textBox5.ReadOnly = true;
-            this.textBox5.Size = new System.Drawing.Size(62, 62);
-            this.textBox5.TabIndex = 4;
-            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.uxWordLetter6.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.uxWordLetter6.Enabled = false;
+            this.uxWordLetter6.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxWordLetter6.Location = new System.Drawing.Point(346, 19);
+            this.uxWordLetter6.MaxLength = 1;
+            this.uxWordLetter6.Name = "uxWordLetter6";
+            this.uxWordLetter6.Size = new System.Drawing.Size(62, 62);
+            this.uxWordLetter6.TabIndex = 4;
+            this.uxWordLetter6.TabStop = false;
+            this.uxWordLetter6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.uxWordLetter6.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextTwist_KeyPress);
             // 
-            // textBox6
+            // uxWordLetter5
             // 
-            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox6.Location = new System.Drawing.Point(278, 19);
-            this.textBox6.MaxLength = 1;
-            this.textBox6.Name = "textBox6";
-            this.textBox6.ReadOnly = true;
-            this.textBox6.Size = new System.Drawing.Size(62, 62);
-            this.textBox6.TabIndex = 5;
-            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.uxWordLetter5.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.uxWordLetter5.Enabled = false;
+            this.uxWordLetter5.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxWordLetter5.Location = new System.Drawing.Point(278, 19);
+            this.uxWordLetter5.MaxLength = 1;
+            this.uxWordLetter5.Name = "uxWordLetter5";
+            this.uxWordLetter5.Size = new System.Drawing.Size(62, 62);
+            this.uxWordLetter5.TabIndex = 5;
+            this.uxWordLetter5.TabStop = false;
+            this.uxWordLetter5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.uxWordLetter5.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextTwist_KeyPress);
             // 
             // uxWordsList
             // 
             this.uxWordsList.FormattingEnabled = true;
+            this.uxWordsList.IntegralHeight = false;
             this.uxWordsList.Location = new System.Drawing.Point(12, 12);
             this.uxWordsList.Name = "uxWordsList";
-            this.uxWordsList.Size = new System.Drawing.Size(282, 472);
+            this.uxWordsList.SelectionMode = System.Windows.Forms.SelectionMode.None;
+            this.uxWordsList.Size = new System.Drawing.Size(282, 477);
             this.uxWordsList.TabIndex = 14;
+            this.uxWordsList.TabStop = false;
+            this.uxWordsList.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextTwist_KeyPress);
             // 
             // TextTwistForm
             // 
@@ -247,12 +281,12 @@
         private System.Windows.Forms.TextBox uxLetter6;
         private System.Windows.Forms.GroupBox uxLettersGroup;
         private System.Windows.Forms.GroupBox uxWordGroup;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox uxWordLetter1;
+        private System.Windows.Forms.TextBox uxWordLetter2;
+        private System.Windows.Forms.TextBox uxWordLetter3;
+        private System.Windows.Forms.TextBox uxWordLetter4;
+        private System.Windows.Forms.TextBox uxWordLetter6;
+        private System.Windows.Forms.TextBox uxWordLetter5;
         private System.Windows.Forms.ListBox uxWordsList;
     }
 }
